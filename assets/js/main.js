e3be24bfd6fef94adc910e888c4e1b25d60664a1
Camila Sáez <cfsaezbravo@gmail.com>
2017-07-20 21:27:23 -0400

    Subiendo archivos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-main.js
+window.addEventListener('scroll', function (e) {
+
+        if (document.documentElement.scrollTop || document.body.scrollTop > 50) {
+            $('nav').attr('style', 'background-color: rgba(175, 60, 229, 0.3)');
+            }
+        else{
+        	$('nav').attr('style', 'background-color: transparent');
+        }
+    });
+
+$(".button-collapse").sideNav();
+
